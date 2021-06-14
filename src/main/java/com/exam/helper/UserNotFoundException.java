d7fe77e7d3c76f9b80f8e5cd5cf34169d2985104
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2021-06-15 00:29:35 +0530

    spring security and jwt auth implemented

--- src/main/java/com/exam/helper/UserNotFoundException.java
+++ src/main/java/com/exam/helper/UserNotFoundException.java
@@ -2,7 +2,7 @@ package com.exam.helper;
 
 public class UserNotFoundException extends Exception {
 
-	private String message;
+	//private String message;
     public UserNotFoundException() {
         super("User does not exist in database");
     }
