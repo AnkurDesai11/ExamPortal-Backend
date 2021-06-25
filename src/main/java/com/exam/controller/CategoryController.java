a9c134de8d20de15cb726fe201e004d471aff217
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2021-06-25 18:24:11 +0530

    added cors

--- src/main/java/com/exam/controller/CategoryController.java
+++ src/main/java/com/exam/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.exam.model.exam.Category;
 import com.exam.service.CategoryService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/category")
 public class CategoryController {
 
